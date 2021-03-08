cff7db759cb893f030a1450d5195e1c3ed8f2394
NadaFraihat <nada997fraihat@gmail.com>
2021-03-08 13:36:49 +0200

    CSS And JS editing

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-alert("Welcome");
-var iphoneOrder = prompt("iPhone 12 Pro Max or iPhone 12 Pro");
-while(iphoneOrder != "iPhone 12 Pro Max" && iphoneOrder !="iPhone 12 Pro")
-{
-    iphoneOrder= prompt("iPhone 12 Pro Max or iPhone 12 Pro");
+
+function userOrder (){
+    alert("Welcome");
+    var iphoneOrder = prompt("iPhone 12 Pro Max or iPhone 12 Pro");
+    while(iphoneOrder != "iPhone 12 Pro Max" && iphoneOrder !="iPhone 12 Pro")
+    {
+        iphoneOrder= prompt("iPhone 12 Pro Max or iPhone 12 Pro");
+    }
+    var typeOfOrder= "";
+    if ( iphoneOrder == "iPhone 12 Pro Max"){
+        typeOfOrder ='<img src="images.jpg">';
+    }
+    else {
+        typeOfOrder ='<img src="images (1).jpg">';
+    }
+    var numOfPhones = prompt("How many iPhones you want?");
+    var order ="";
+    for(i=0; i<numOfPhones ; i++){
+    order = order+typeOfOrder ;
+    }
+    document.write(order);
+
 
 }
+userOrder();
+function paying(){
+    var Paying = prompt("What do you want, Cash or Paypal? ");
+    while(Paying != "Cash" && Paying !="Paypal")
+    {
+        Paying= prompt("Cash or Paypal?");
+    }
+    var typeOfPaying= "";
+    if (Paying == "Cash"){
+        alert("Please visit our shop!");
+    }
+    else {
+        typeOfPaying = "<a href=https://www.paypal.com/jo/home>Click here please</a>";
 
-var typeOfOrder= "";
-if ( iphoneOrder == "iPhone 12 Pro Max"){
-    typeOfOrder ='<img src="images.jpg">';
+    }
+    document.write(typeOfPaying);
 }
-else {
-    typeOfOrder ='<img src="images (1).jpg">';
-}
-var numOfPhones = prompt("How many iPhones you want?");
-var order ="";
-for(i=0; i<numOfPhones ; i++){
-order = order+typeOfOrder ;
-}
-document.write(order);
+paying();
+
